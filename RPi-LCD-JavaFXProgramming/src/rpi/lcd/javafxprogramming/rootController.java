2efc5d54fbb431f858c344dbe2112e64cdf3d59d
kyeongsikkim <kimks3131@naver.com>
2017-05-12 09:30:14 +0900

    업로드

--- RPi-LCD-JavaFXProgramming/src/rpi/lcd/javafxprogramming/rootController.java
+++ RPi-LCD-JavaFXProgramming/src/rpi/lcd/javafxprogramming/rootController.java
@@ -5,31 +5,52 @@
  */
 package rpi.lcd.javafxprogramming;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.StackPane;
 
 /**
+ * FXML Controller class
  *
  * @author Administrator
  */
-public class rootController implements Initializable {
-	
+public class RootController implements Initializable {
+
 	@FXML
-	private Label label;
-	
+	private StackPane stack;
 	@FXML
-	private void handleButtonAction(ActionEvent event) {
-		System.out.println("You clicked me!");
-		label.setText("Hello World!");
-	}
-	
+	private Slider slider1;
+	@FXML
+	private Button btnCh;
+
+	/**
+	 * Initializes the controller class.
+	 */
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		// TODO
+		btnCh.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				
+				try {
+					stack.getChildren().clear();
+					Parent parent=FXMLLoader.load(getClass().getResource("xx.fxml"));
+					stack.getChildren().add(parent);
+				} catch (IOException ex) {
+					
+				}
+				
+			}
+		});
 	}	
 	
 }
